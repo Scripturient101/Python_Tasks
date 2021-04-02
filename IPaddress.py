2f004a561f14a882d1bc72aae7ed16c0818ef33e
Shruti Chauhan <81686454+Scripturient101@users.noreply.github.com>
2021-04-02 14:58:11 +0530

    Update IPaddress.py

--- IPaddress.py
+++ IPaddress.py
@@ -1,5 +1,7 @@
 #Python program to find the IP Address of the system
-import socket    
+import socket  
+                  #gethostname() -- return the current hostname
 hostname = socket.gethostname()    
+                  #gethostbyname() -- map a hostname to its IP number
 IPAddr = socket.gethostbyname(hostname)     
 print( IPAddr)
